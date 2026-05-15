0081baa066feaffd31c14ca0c3f041651d733242
divani <vanileli142@gmail.com>
2026-05-15 12:45:29 +0700

    saving main

--- pertemuan 9.R
+++ pertemuan 9.R
@@ -3,17 +3,3 @@ mean(mtcars$mpg)
 sd(mtcars$mpg)
 mtcars
 max(mtcars$disp)
-<<<<<<< HEAD
-
-hist(mtcars$disp)
-=======
-min(mtcars$hp)
-
-boxplot(mtcars$disp)
-hist(mtcars$cyl)
->>>>>>> branch-baru
-
-mtcars
-summary(mtcars$qsec)
-sd(mtcars$gear)
-sd(mtcars$qsec)
