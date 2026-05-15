db12c8d4be0025f512b2371c1b78b8d7246ac6f1
divani <vanileli142@gmail.com>
2026-05-15 12:44:23 +0700

    testing new branch

--- pertemuan 9.R
+++ pertemuan 9.R
@@ -1,19 +1,1 @@
 summary(mtcars)
-mean(mtcars$mpg)
-sd(mtcars$mpg)
-mtcars
-max(mtcars$disp)
-<<<<<<< HEAD
-
-hist(mtcars$disp)
-=======
-min(mtcars$hp)
-
-boxplot(mtcars$disp)
-hist(mtcars$cyl)
->>>>>>> branch-baru
-
-mtcars
-summary(mtcars$qsec)
-sd(mtcars$gear)
-sd(mtcars$qsec)
